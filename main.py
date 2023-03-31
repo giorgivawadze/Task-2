00a16a016ad1a252ba67da79345d85ce1dbcc955
giorgivawadze <116506482+giorgivawadze@users.noreply.github.com>
2023-03-31 22:53:20 +0400

    s

---
+++ main.py
@@ -0,0 +1,17 @@
+
+Cars = [
+  car := ['Bugatti', '2m'],
+  car := ['Lamborghini', '571k'],
+  car := ['Porsche', '100k'],
+  car := ['Mercedes', ' 45k'],
+  car := ['BMW', '5k']
+
+]
+
+for car in Cars:
+    print(car[0], car[1])
+
+
+
+
+
