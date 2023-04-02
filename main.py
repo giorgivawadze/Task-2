7340a821fe04135eae0dbcab1b6d983797338557
giorgivawadze <116506482+giorgivawadze@users.noreply.github.com>
2023-04-02 19:15:39 +0400

    second option too

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 
+
+# first 
 Cars = [
-  car := ['Bugatti', '2m'],
-  car := ['Lamborghini', '571k'],
-  car := ['Porsche', '100k'],
-  car := ['Mercedes', ' 45k'],
-  car := ['BMW', '5k']
+  car := ['Bugatti-', '2m'],
+  car := ['Lamborghini-', '571k'],
+  car := ['Porsche-', '100k'],
+  car := ['Mercedes-', ' 45k'],
+  car := ['BMW-', '5k']
 
 ]
 
@@ -15,3 +17,14 @@ for car in Cars:
 
 
 
+
+#second 
+
+
+
+phone = 'iphone 12pro', 'iphone X, ', 'samsung A51', 'samsung A52'
+price = ['iphone 12pro = 1k $, iphone X= 550$, samsung A51= 400$, samsung A52= 450$']
+
+
+for phone in price :
+    print (phone)
